e2d6b19cdd6ba2a7dfad1baa883afd9564fb44fd
evillari <300749230@cencol.ca>
2016-09-13 10:39:16 -0400

    Initial Commit

---
+++ Scripts/core/game.ts
@@ -0,0 +1,30 @@
+console.log("Game is starting!");
+
+class Person {
+    protected _name : string;
+
+    constructor(name:string){
+        this._name = name;
+    }
+
+    public speak() : void {
+        console.log(this._name + " says magical hello!");
+    }
+}
+class Student extends Person{
+    private _studentNum : number;
+
+    constructor(name:string,studentNum:number){
+        super(name);
+        this._studentNum = studentNum;  
+    }
+
+    public studies() : void {
+        console.log(this._name + " is studying.")
+    }
+}
+var person : Person = new Person("Harry Pottah");
+person.speak();
+var student : Student = new Student("Hermione Granger", 0987654321);
+student.speak();
+student.studies();
